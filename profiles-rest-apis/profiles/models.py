36689981edd93543b0ca4f530b80d0ac882ed847
= <vishal.tandon568@gmail.com>
2023-04-09 16:28:48 +0530

    added custom user profile class and set the setting for AUTH_USER_MODEL

--- profiles-rest-apis/profiles/models.py
+++ profiles-rest-apis/profiles/models.py
@@ -1,3 +1,56 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """create a new user profile"""
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        # make second part case insensitive
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        # set_password will encrypt the the password
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email,name,password)
+        user.is_superuser = True
+        user.is_staff =  True
+        user.save(using=self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database models for user in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # objects = UsersProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """retrieve full name of the user"""
+        return self.name
+
+    def get_short_name(self):
+        """retrieve self name of user"""
+        return self.name
+
+    def __str__(self):
+        """return string represenation of the user"""
+        return self.email
